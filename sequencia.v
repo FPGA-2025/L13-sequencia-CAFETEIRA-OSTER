8682772ad2e072cdc16eb67f8a2214483e3ff9c1
Yuri Bernardo <yuribernardosie@gmail.com>
2025-05-03 14:59:42 -0300

    Tentativa 1 - insucessos ao tentar receber "encontrado == 1"

--- sequencia.v
+++ sequencia.v
@@ -10,6 +10,33 @@ module Sequencia (
 
     output reg encontrado
 );
-// Seu codigo aqui    
+
+    reg [7:0] palavra_atual;
+    reg [2:0] x; 
+
+    always @(posedge clk) begin
+        if (!rst_n) begin
+            palavra_atual <= 8'b0;
+            encontrado <= 1'b0;
+            x <= 7;
+        end else if (setar_palavra) begin
+            // Armazenar palavra
+            palavra_atual <= palavra;
+            encontrado <= 0;
+            x <= 7;
+        end else if (start && encontrado == 1'b0) begin
+            // Comparar palavra
+            if (palavra_atual[x] == bit_in) begin
+                x <= x - 1;
+                if (x == 0) begin
+                    encontrado <= 1'b1;
+                    x <= 7;
+                end
+            end else begin
+                x <= 7;
+            end
+        end
+    end
+    
 
 endmodule
