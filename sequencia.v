12eefb03cc8fa663c43f4bbf8cfb7db785907e2e
Yuri Bernardo <yuribernardosie@gmail.com>
2025-05-05 19:36:51 -0300

    Tarefa realiza

--- sequencia.v
+++ sequencia.v
@@ -12,29 +12,32 @@ module Sequencia (
 );
 
     reg [7:0] palavra_atual;
-    reg [2:0] x; 
+    reg [3:0] x;
+    reg stts;
 
     always @(posedge clk) begin
         if (!rst_n) begin
             palavra_atual <= 8'b0;
             encontrado <= 1'b0;
-            x <= 7;
+            x <= 8;
+            stts <= 1'b0;
         end else if (setar_palavra) begin
             // Armazenar palavra
             palavra_atual <= palavra;
             encontrado <= 0;
-            x <= 7;
-        end else if (start && encontrado == 1'b0) begin
+            x <= 8;
+        end else if(start) begin
+            stts <= 1'b1;
+        end if (x == 0) begin
+            encontrado <= 1'b1;
+            x <= 8;
+        end else if (stts && encontrado == 1'b0) begin
             // Comparar palavra
-            if (palavra_atual[x] == bit_in) begin
+            if (palavra_atual[x - 1] == bit_in) begin
                 x <= x - 1;
-                if (x == 0) begin
-                    encontrado <= 1'b1;
-                    x <= 7;
-                end
             end else begin
-                x <= 7;
-            end
+                x <= 8;
+            end 
         end
     end
     
