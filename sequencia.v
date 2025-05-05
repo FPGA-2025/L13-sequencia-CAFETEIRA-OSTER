df64ab8b30db049f47704e134f65d448de6eda44
Yuri Bernardo <yuribernardosie@gmail.com>
2025-05-05 19:48:06 -0300

    alterações feitas

--- sequencia.v
+++ sequencia.v
@@ -21,25 +21,26 @@ module Sequencia (
             encontrado <= 1'b0;
             x <= 8;
             stts <= 1'b0;
-        end else if (setar_palavra) begin
-            // Armazenar palavra
-            palavra_atual <= palavra;
-            encontrado <= 0;
-            x <= 8;
-        end else if(start) begin
-            stts <= 1'b1;
-        end if (x == 0) begin
-            encontrado <= 1'b1;
-            x <= 8;
-        end else if (stts && encontrado == 1'b0) begin
-            // Comparar palavra
-            if (palavra_atual[x - 1] == bit_in) begin
-                x <= x - 1;
-            end else begin
+        end else begin 
+            if (setar_palavra) begin
+                // Armazenar palavra
+                palavra_atual <= palavra;
+                encontrado <= 0;
                 x <= 8;
-            end 
+            end else if(start) begin
+                stts <= 1'b1;
+            end if (x == 0) begin
+                encontrado <= 1'b1;
+                x <= 8;
+            end else if (stts && encontrado == 1'b0) begin
+                // Comparar palavra
+                if (palavra_atual[x - 1] == bit_in) begin
+                    x <= x - 1;
+                end else begin
+                    x <= 8;
+                end 
+            end
         end
     end
-    
 
 endmodule
